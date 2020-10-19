a6144b06fd271d6dd174257da59f1472a2f194e0
Tim Voets <tim.voets@asr.nl>
2020-10-19 10:13:22 +0200

    flake sugesstions

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import requests_cache
 from src.utils import AzCommandHandler, choice_menu
 from src.azclitools import commands
 
-requests_cache.install_cache(cache_name='az_cache', backend='sqlite', expire_after=180)
+requests_cache.install_cache(
+    cache_name='az_cache',
+    backend='sqlite',
+    expire_after=180
+)
+
 
 def format_name(name: str) -> str:
     return " ".join(map(lambda s: s.capitalize(), name.split('_')))
@@ -13,13 +18,11 @@ if __name__ == "__main__":
     choice = choice_menu([format_name(i) for i in commands])
 
     execute = commands[list(commands.keys())[choice]]
-    
+
     cli = AzCommandHandler()
-    cli.command=execute['az_data_command']
+    cli.command = execute['az_data_command']
     filtered = map(execute['filter'], cli.result)
 
     menu_result = choice_menu(filtered)
     print(filtered)
     print(menu_result)
-
-
--- src/azclitools.py
+++ src/azclitools.py
@@ -1,7 +1,7 @@
 commands = {
     "switch_default_subscription": [
         ("az_data_command", "account list"),
-        ("filter", lambda x: {k:v for k,v in x.items() if k in ['name']}),
+        ("filter", lambda x: {k: v for k, v in x.items() if k in ['name']}),
         ("menu", "name"),
         ("az_set_command", "account set --subscription {name}")
     ]
